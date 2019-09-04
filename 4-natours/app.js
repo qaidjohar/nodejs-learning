04bec3aad9f5b9f997ac1f17ea98f7e22a7791d2
Qaidjohar <qaidjawadwala@gmail.com>
2019-09-04 09:34:59 +0530

    CURD Routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -28,6 +28,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID  '
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -47,6 +68,34 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.param.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID  '
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour Here>'
+    }
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.param.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID  '
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
 
 const port = 3000;
