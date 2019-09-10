13dbf02f0a04d18a313052786660831fa90e2f1f
Qaidjohar <qaidjawadwala@gmail.com>
2019-09-10 23:42:50 +0530

    Sample Insert into DB

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -19,7 +19,37 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-// console.log(process.env);
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a Name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a Price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Part Camper',
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR:', err);
+  });
 
 //Server Start
 const port = process.env.PORT || 3000;
