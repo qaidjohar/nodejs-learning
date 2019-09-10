52f0f7e4cdf4ed1feaa6da06036c5d080ef7f676
Qaidjohar <qaidjawadwala@gmail.com>
2019-09-11 00:12:09 +0530

    gettingStartedWithMongoose

---
+++ 4-natours/models/tourModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a Name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a Price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -20,37 +20,6 @@ mongoose
     console.log(err);
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A Tour must have a Name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A Tour must have a Price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Part Camper',
-  price: 497
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('ERROR:', err);
-  });
-
 //Server Start
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
